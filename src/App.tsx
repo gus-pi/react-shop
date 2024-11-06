91c05e574ee305032054212701fe221e8c755ce3
PieMenard <pinedo.m.gustavo@gmail.com>
2024-11-06 00:10:07 -0400

    add product edit page and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
 import ProductList from './pages/admin/products/ProductList';
 import CreateProduct from './pages/admin/products/CreateProduct';
+import EditProduct from './pages/admin/products/EditProduct';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/admin/products" element={<ProductList />} />
         <Route path="/admin/products/create" element={<CreateProduct />} />
+        <Route path="/admin/products/edit/:id" element={<EditProduct />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
